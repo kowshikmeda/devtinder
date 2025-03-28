9b7739de68032822103602dc29cfc0d746d046d8
kowshikmeda <medakowshik8@gmail.com>
2025-03-28 13:01:47 +0530

    improved footer style

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,42 +1,44 @@
-import React, { useEffect } from 'react'
-import Navbar from './Navbar'
-import { Outlet, useNavigate } from 'react-router-dom'
-import Footer from './Footer'
-import axios from 'axios'
-import { BASE_URL } from '../../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addUser } from '../../utils/userSlice'
+import React, { useEffect } from 'react';
+import Navbar from './Navbar';
+import { Outlet, useNavigate } from 'react-router-dom';
+import Footer from './Footer';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../../utils/userSlice';
+
 const Body = () => {
   const dispatch = useDispatch();
-  const navigate=useNavigate();
-  const userData=useSelector((store)=>store.user);
-  const fetchUser=async()=>{
-    try{
-      const res=await axios.get(BASE_URL+"/profile/view",{withCredentials:true});
-     // console.log(res.data);
-      dispatch(addUser(res.data))
-    }catch(err){
-      if(err.status===401){
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", { withCredentials: true });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
         navigate("/login");
       }
-     
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
+
   useEffect(() => {
-    if(!userData){
+    if (!userData) {
       fetchUser();
     }
-   
   }, []);
+
   return (
-    <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
-     </div>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-grow pb-[30vh]"> {/* Leaves space for the fixed footer */}
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-}
-
-export default Body
+export default Body;
