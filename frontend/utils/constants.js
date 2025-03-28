878b51969b706ca65efaa5719d688eb4412e59fc
kowshikmeda <medakowshik8@gmail.com>
2025-03-28 17:45:59 +0530

    api changes in constants

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:3000":"/";
+export const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:3000":"https://devtinder-jspt.onrender.com";
