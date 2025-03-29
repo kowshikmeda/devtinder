2c4ea4da9a1304cfe53ac7de9108014a3c6d5060
kowshikmeda <medakowshik8@gmail.com>
2025-03-29 20:12:31 +0530

    order changes

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,12 @@ const requestRouter = require('./routes/request');
 const userRouter = require('./routes/user');
 dotenv.config();
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: ["http://localhost:5173", "https://devtinder-k8t8.onrender.com"],
+    credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use("/",authRouter);
-app.use("/",profileRouter);
-app.use("/",requestRouter);
-app.use("/",userRouter);
-
-let port=process.env.PORT;
 if(process.env.MODE==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("*",(req,res)=>{
@@ -29,6 +24,13 @@ if(process.env.MODE==="production"){
     })
 
 }
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
+app.use("/",userRouter);
+
+let port=process.env.PORT;
+
 connectDB().then(()=>{
     app.listen(port,()=>{   
         console.log(`Server is running on ${port} `);
--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,3 +1,3 @@
 
-console.log("port mode:",import.meta.env.MODE);
-export const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:3000":"";
+
+export const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:3000":"https://devtinder-k8t8.onrender.com";
